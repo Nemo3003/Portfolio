fb8dcb5aa33f33d8cc8095063ed391c029ea51e8
Nemo3003 <ulimarian2000@gmail.com>
2022-07-11 09:54:47 -0300

    my-new-portfolio

---
+++ ulises-portfolio/src/components/NavBar.jsx
@@ -0,0 +1,15 @@
+import '../sass/styles.css';
+
+
+export default function Navbar(){
+    return <nav className="navigation">
+        <ul className='list'>
+            <li className='items'><a href="#">Home</a></li>
+            <li className='items'> <a href='#'>&nbsp;Skills</a></li>
+            <li className='items'> <a href='#'>&nbsp;About Me</a></li>
+            <li className='items'><a href='#'>&nbsp;Projects</a></li>
+            <li className='items'><a href='#'>&nbsp;Contact Me</a></li>
+        </ul>
+        <button className="btn">Menu</button>
+    </nav>
+}
---
+++ ulises-portfolio/src/helper/FetchJson.js
@@ -0,0 +1,2 @@
+//Fetch the projects.json file in data and get all the information
+//and display it in the page.
