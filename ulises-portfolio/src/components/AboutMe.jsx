4bcd0ed8744789bfdddf55e30938c63d1b7c6907
Nemo3003 <ulimarian2000@gmail.com>
2022-09-13 12:57:11 -0300

    [UPD] Better UI

--- ulises-portfolio/src/components/AboutMe.jsx
+++ ulises-portfolio/src/components/AboutMe.jsx
@@ -1,14 +1,15 @@
-import React from 'react'
-
 export default function AboutMe() {
     return(
         <div className="about-me">
             <div className='pic-about'>
-                <img src="../img/image.jpg" alt="my image" />
+                
             </div>
             <div className='about-me-text'>
                 <h1>About Me</h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis quas vel odio eligendi rerum quos reprehenderit odit atque vero excepturi deleniti, <br /> repellat numquam amet blanditiis ducimus vitae nemo eveniet provident!</p>
+                <p>I am Ulises, from Argentina. I am currently studying Applied Technology and a few other things <br/>
+                I'm a proactive Software Developer eager to learn and apply all my skills!<br/> I love challenges and absolutely love working with a team! 
+                👨‍🌾​My ideal workplace is a place in which I am constantly being challenged!<br/> A place in which my skills can be improved and there is a nice environment in which we are all respecting and helping each other!!
+                </p>
             </div>
         </div>
     )
--- ulises-portfolio/src/components/Header.jsx
+++ ulises-portfolio/src/components/Header.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
 import '../sass/styles.css';
+import me from '../assets/me.jpg'
 
 export default function Header() {
     return(
         <div className="row">
-
-        <div className='col-md-8'>
+        <div className='col-md-12'>
             <p>Hello, I'm </p><h1><span className='name'>Ulises Mariano Melgarejo</span>.</h1>
             <p>and I am a Software Developer</p>
         </div>
         <div className='col-md-4'>
-            <img src="../img/image.jpg" alt="Me" />
+        <div className='pic-about'>
+                <img width='324vh' src={me} alt="my image" />    
+            </div>
         </div>
         </div>
     )
